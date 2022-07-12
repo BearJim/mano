af056be29891983b959d233bd1f7567a01007b6d
bearjim <b309554039.cs09g@nctu.edu.tw>
2022-07-12 14:20:15 +0000

    yaml 0712

--- vnf_create.sh
+++ vnf_create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-openstack vnf create --vnfd-name mongo mongo --vim-name chris_vim --description "mongodb"
-#openstack vnf create --vnfd-name upf upf --vim-name chris_vim --description "upf"
-openstack vnf create --vnfd-name upf1 upf1 --vim-name chris_vim --description "upf1"
+openstack vnf create --vnfd-name mongo mongo --vim-name vim --description "mongodb"
+openstack vnf create --vnfd-name upf upf --vim-name vim --description "upf"
+# openstack vnf create --vnfd-name upf1 upf1 --vim-name chris_vim --description "upf"
 #sleep 5
 #openstack vnf create --vnfd-name upf2 upf2 --vim-name chris_vim --description "upf2"
 #sleep 5
@@ -12,19 +12,25 @@ openstack vnf create --vnfd-name upf1 upf1 --vim-name chris_vim --description "u
 
 sleep 10
 
-openstack vnf create --vnfd-name nrf nrf --vim-name chris_vim --description "nrf"
+openstack vnf create --vnfd-name nrf nrf --vim-name vim --description "nrf"
 sleep 10
-openstack vnf create --vnfd-name amf amf --vim-name chris_vim --description "amf"
+openstack vnf create --vnfd-name amf amf --vim-name vim --description "amf"
 sleep 10
-openstack vnf create --vnfd-name smf1 smf1 --vim-name chris_vim --description "smf"
+openstack vnf create --vnfd-name amf1 amf1 --vim-name vim --description "amf"
 sleep 10
-openstack vnf create --vnfd-name udr udr --vim-name chris_vim --description "udr"
+openstack vnf create --vnfd-name amf2 amf2 --vim-name vim --description "amf"
 sleep 10
-openstack vnf create --vnfd-name pcf pcf --vim-name chris_vim --description "pcf"
+openstack vnf create --vnfd-name smf smf --vim-name vim --description "smf"
 sleep 10
-openstack vnf create --vnfd-name udm udm --vim-name chris_vim --description "udm"
+openstack vnf create --vnfd-name udr udr --vim-name vim --description "udr"
 sleep 10
-openstack vnf create --vnfd-name nssf nssf --vim-name chris_vim --description "nssf"
+openstack vnf create --vnfd-name pcf pcf --vim-name vim --description "pcf"
 sleep 10
-openstack vnf create --vnfd-name ausf ausf --vim-name chris_vim --description "ausf"
+openstack vnf create --vnfd-name udm udm --vim-name vim --description "udm"
+sleep 10
+openstack vnf create --vnfd-name nssf nssf --vim-name vim --description "nssf"
+sleep 10
+openstack vnf create --vnfd-name ausf ausf --vim-name vim --description "ausf"
+sleep 10
+openstack vnf create --vnfd-name lb lb --vim-name vim --description "lb"
 
